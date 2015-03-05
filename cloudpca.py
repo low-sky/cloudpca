63754845e8ec9b945eb52b3ff8617adc735908b7
low_sky <erosolow@ualberta.ca>
2015-03-05 15:10:43 -0700

    Added some more functions; fixed column-wise means

--- cloudpca.py
+++ cloudpca.py
@@ -2,6 +2,7 @@ import numpy as np
 import astropy.io.fits as fits
 from spectral_cube import SpectralCube
 import pdb
+import numpy.fft as fft
 
 # Code block to parse string / SpectralCube into PCA
 #     if isinstance(input,SpectralCube):
@@ -13,10 +14,28 @@ import pdb
 #             raise
 #     else:
 #         raise NotImplementedError
-    
-def EigenImages(evec,cube,nImg = 10):
+
+def AutoCorrelateImages(imageList):
+    acorList = []
+    for image in imageList:
+        fftx = fft.fft2(image)
+        fftxs = np.conjugate(fftx)
+        acor = fft.fftshift(fft.ifft2((fftx-fftx.mean())*(fftxs-fftxs.mean())))
+        acorList.append(acor.real)
+    return(acorList)
+
+def AutoCorrelateSpectrum(evec,nScales = 10):
+    acorList = []
+    for idx in range(nScales):
+        fftx = fft.fft(evec[:,idx])
+        fftxs = np.conjugate(fftx)
+        acor = fft.fftshift(fft.ifft((fftx-fftx.mean())*(fftxs-fftxs.mean())))
+        acorList.append(acor.real)
+    return(acorList)
+
+def EigenImages(evec,cube,nScales = 10):
     imageList = []
-    for idx in range(nImg):
+    for idx in range(nScales):
         thisImage = np.zeros((cube.shape[1],cube.shape[2]))
         for channel in range(cube.shape[0]):
             thisImage +=np.nan_to_num(cube[channel,:,:].value*evec[channel,idx])
@@ -25,11 +44,13 @@ def EigenImages(evec,cube,nImg = 10):
 
 def pca(cube):
     PCAMatrix = np.zeros((cube.shape[0],cube.shape[0]))
-    MeanOfData = cube.mean()
+    ChannelMeans = np.zeros((cube.shape[0]))
+    for i in range(cube.shape[0]):
+        ChannelMeans[i] = np.nanmean(cube[i,:,:].value)
     for i in range(cube.shape[0]):
         for j in range(i):
-            PCAMatrix[i,j] = np.nanmean(((cube[i,:,:]-MeanOfData)*(cube[j,:,:]-MeanOfData)).value)
-        PCAMatrix[i,i] = np.nanmean(((cube[i,:,:]-MeanOfData)*(cube[i,:,:]-MeanOfData)).value)
+            PCAMatrix[i,j] = np.nanmean(((cube[i,:,:].value-ChannelMeans[i])*(cube[j,:,:].value-ChannelMeans[j])))
+        PCAMatrix[i,i] = np.nanmean((cube[i,:,:].value-ChannelMeans[i])**2)
     PCAMatrix = PCAMatrix + np.transpose(PCAMatrix)
     # Correct elements on the diagonal for the doubling in the transpose-and-add
     PCAMatrix[range(cube.shape[0]),range(cube.shape[0])] = \
--- sample.py
+++ sample.py
@@ -1,5 +1,8 @@
 from spectral_cube import SpectralCube
 from cloudpca import cloudpca
-s = SpectralCube.read('~/Dropbox/AstroStatistics/ngc1333.13co.fits')
+s = SpectralCube.read('/Users/erosolo/Dropbox/AstroStatistics/ngc1333.13co.fits')
 evals,evec,matrix = cloudpca.pca(s)
 ll = cloudpca.EigenImages(evec,s)
+acorImg = cloudpca.AutoCorrelateImages(ll)
+acorSpec = cloudpca.AutoCorrelateSpectrum(evec)
+
